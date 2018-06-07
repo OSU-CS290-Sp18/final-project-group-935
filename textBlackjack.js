af96c2dd33216a22e39fe43156135f2424382e21
robeshel <38081241+robeshel@users.noreply.github.com>
2018-06-06 23:08:08 -0700

    Update textBlackjack.js

--- textBlackjack.js
+++ textBlackjack.js
@@ -3,9 +3,16 @@
 
 //Math.floor((Math.random() * 13) + 1); = return a random number between 1-13
 
+//button variables
+var newGame = document.getElementById('newGame');
 var dealCards = document.getElementById('hit');
-var stayCards = document.getElementById('stay');
+var standCards = document.getElementById('stand');
+var doubleDownCards = document.getElementById('doubleDown');
+var splitCards = document.getElementById('split');
+var imperialCredit = document.getElementById('imperialCredit');
+var imperialCreditVal = imperialCredit.value;
 
+//normal variables
 var dealerCard1;
 var dealerCard2;
 var playerCard1;
@@ -13,38 +20,80 @@ var playerCard2;
 var dealerTotal;
 var playerTotal;
 var newCardNum;
+var switchingVar;
+
+imperialCreditVal = 1000;			//NOT WORKING, WHERE I LEFT OFF
+
+newGame.style.display = "block";
+dealCards.style.display = "none";			//This block of code appears often
+standCards.style.display = "none";			//It is just blocking all buttons except newGame when not in a game
+doubleDownCards.style.display = "none";		//Or it showing all the buttons except newGame when in game
+splitCards.style.display = "none";
+
+newGame.onclick = function() {					//when newGame button is pressed
+
+	newGame.style.display = "none";
+	dealCards.style.display = "block";
+	standCards.style.display = "block";
+	doubleDownCards.style.display = "block";
+	splitCards.style.display = "block";
+
+	dealerTotal = 0;
+	playerTotal = 0;
+
+	//dealer gets 2 cards, player gets 2 cards
+	//if the card is equal to 11, 12, or 13, the value is replaced with 10
+	dealerCard1 = Math.floor((Math.random() * 13) + 1);
+	if (dealerCard1 > 10) {
+		dealerCard1 = 10;
+	}
+
+	dealerCard2 = Math.floor((Math.random() * 13) + 1);
+	if (dealerCard2 > 10) {
+		dealerCard2 = 10;
+	}
+
+	if ( (dealerCard1 == 1) || (dealerCard2 == 1) ) {		//if dealer gets an ace, dealerTotal+10
+		dealerTotal = 10;
+	}
+
+	playerCard1 = Math.floor((Math.random() * 13) + 1);
+	if (playerCard1 > 10) {
+		playerCard1 = 10;
+	}
+	console.log("playerCard1 = " + playerCard1);
+
+	playerCard2 = Math.floor((Math.random() * 13) + 1);
+	if (playerCard2 > 10) {
+		playerCard2 = 10;
+	}
+
+	console.log("playerCard2 = " + playerCard2);
+	console.log("");
 
 
-dealerCard1 = Math.floor((Math.random() * 13) + 1);
-dealerCard2 = Math.floor((Math.random() * 13) + 1);
+	alert("dealer's first card is: " + dealerCard1);			//showing the player the dealer's first card
 
-playerCard1 = Math.floor((Math.random() * 13) + 1);
-console.log("playerCard1 = " + playerCard1);
+	dealerTotal = dealerTotal + dealerCard1 + dealerCard2;		//calculating totals for player and dealer after first two cards are drawn
+	playerTotal = playerCard1 + playerCard2;
+	console.log("dealer total = " + dealerTotal);
+	console.log("playerTotal = " + playerTotal);
+	console.log("");
 
-playerCard2 = Math.floor((Math.random() * 13) + 1);
-console.log("playerCard2 = " + playerCard2);
+	if ((playerCard1 + playerCard2) == 21) {		//if player gets blackjack, automatic win
+		console.log("player wins.");
+		alert("player wins.");
 
-alert("dealer's first card is: " + dealerCard1);
+		newGame.style.display = "block";
+		dealCards.style.display = "none";
+		standCards.style.display = "none";
+		doubleDownCards.style.display = "none";
+		splitCards.style.display = "none";
+	}
 
-dealerTotal = dealerCard1 + dealerCard2;
-playerTotal = playerCard1 + playerCard2;
-console.log("dealer total = " + dealerTotal);
-console.log("");
-console.log("playerTotal = " + playerTotal);
-
-if (ifBust(dealerTotal) == true) {
-	console.log("dealer bust.");
-	alert("dealer bust.");
 }
-else if (ifBust(playerTotal) == true) {
-	console.log("player bust.");
-	alert("player bust.");
-}
-else {}
 
-
-
-function ifBust(number) {
+function ifBust(number) {			//returns true if player/dealer is bust, returns false if player/dealer isn't
 
 	if (number > 21){
 		return true;
@@ -54,35 +103,88 @@ function ifBust(number) {
 	}
 }
 
-
-dealCards.onclick = function() {
+dealCards.onclick = function() {							//when hit button is pressed
     newCardNum = Math.floor((Math.random() * 13) + 1);
+    if (newCardNum > 10) {
+		newCardNum = 10;
+	}
+
 	console.log("newCardNum = " + newCardNum);
 	playerTotal = playerTotal + newCardNum;
 	console.log("");
 	console.log("player total = " + playerTotal);
 	console.log("");
 
-	if (ifBust(playerTotal) == true) {
-	console.log("player bust.");
-	alert("player bust.");
+	if (newCardNum == 1) {						//if new card number is an ace, switch it with playerCard1 so that the next if statement will work
+		switchingVar = newCardNum;				//works regardless if player previously had an ace
+		newCardNum = playerCard1;
+		playerCard1 = switchingVar;
 	}
-	else {}
+
+	if ( ( (playerCard1 == 1) || (playerCard2 == 1) ) && (playerTotal + 10 == 21) ) {			//if player has an ace, and playerTotal + 10 would equal 21, player wins
+		console.log("player wins.");
+		alert("player wins.");
+
+		newGame.style.display = "block";
+		dealCards.style.display = "none";
+		standCards.style.display = "none";
+		doubleDownCards.style.display = "none";
+		splitCards.style.display = "none";
+	}
+
+	if (ifBust(playerTotal) == true) {				//checking if new card made player go bust
+		console.log("player bust.");
+		alert("player bust.");
+
+		newGame.style.display = "block";
+		dealCards.style.display = "none";
+		standCards.style.display = "none";
+		doubleDownCards.style.display = "none";
+		splitCards.style.display = "none";
+	}
+	else if (playerTotal == 21) {
+		console.log("player wins.");
+		alert("player wins.");
+
+		newGame.style.display = "block";
+		dealCards.style.display = "none";
+		standCards.style.display = "none";
+		doubleDownCards.style.display = "none";
+		splitCards.style.display = "none";
+	}
 }
 
-stayCards.onclick = function() {
+standCards.onclick = function() {			//when stand button is pressed
     newCardNum = Math.floor((Math.random() * 13) + 1);
 
-	if (playerTotal > dealerTotal) {
-	console.log("player wins.");
-	alert("player wins.");
+	if (playerTotal > dealerTotal) {					//if player has a higher score, player wins
+		console.log("player wins.");
+		alert("player wins.");
+
+		newGame.style.display = "block";
+		dealCards.style.display = "none";
+		standCards.style.display = "none";
+		doubleDownCards.style.display = "none";
+		splitCards.style.display = "none";
 	}
-	else if (playerTotal == dealerTotal) {
+	else if (playerTotal == dealerTotal) {				//if player and dealer have same score, its a tie
 		console.log("draw.");
 		alert("draw.");
+
+		newGame.style.display = "block";
+		dealCards.style.display = "none";
+		standCards.style.display = "none";
+		doubleDownCards.style.display = "none";
+		splitCards.style.display = "none";
 	}
-	else {
+	else {												//if dealer has a higher score than player, dealer wins
 		console.log("dealer wins.");
 		alert("dealer wins.");
+
+		newGame.style.display = "block";
+		dealCards.style.display = "none";
+		standCards.style.display = "none";
+		doubleDownCards.style.display = "none";
+		splitCards.style.display = "none";
 	}
 }
