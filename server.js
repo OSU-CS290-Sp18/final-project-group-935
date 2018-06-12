a7f6f22817625d5fb9903ae2517a2fc0daefc6fc
Huardd <32319836+Huardd@users.noreply.github.com>
2018-06-12 11:36:00 -0700

    fixed server.js and index.html

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
+// server.js
 
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/textBlackJack.html', function(req, res){
+	res.status(200).sendFile(path.join(__dirname, 'textBlackJack.html'));
+});
+
+app.get('/textBlackJack.js', function(req, res){
+	res.status(200).sendFile(path.join(__dirname, 'textBlackJack.js'));
+});
+
+app.get('/BlackJackRules.html', function(req, res){
+	res.status(200).sendFile(path.join(__dirname, 'BlackJack Rules.html'));
+});
+
+app.get('/index.js', function(req, res){
+	res.status(200).sendFile(path.join(__dirname, 'index.js'));
+});
+
+
+app.get('*', function(req, res){
+	res.status(404).sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, function(){
+	console.log("server is on port 3000");
+});
