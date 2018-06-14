c2d02564ac38fe19a2f22f96783b40c0ec296f42
Huardd <32319836+Huardd@users.noreply.github.com>
2018-06-14 02:57:47 -0700

    upload

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 var game_button = document.getElementById('start');
 var rule_button = document.getElementById('rules');
+var head_home_button = document.getElementById('headhome');
+var head_game_button = document.getElementById('headstart');
+var head_rule_button = document.getElementById('headrules');
 
 function start_game(){
 	window.location.href='/textBlackJack.html';
@@ -11,5 +14,12 @@ function rule_page(){
 	window.location.href='/BlackJackRules.html';
 }
 
+function go_home(){
+	window.location.href='/';
+}
+
+head_home_button.addEventListener('click', go_home);
+head_rule_button.addEventListener('click', rule_page);
 rule_button.addEventListener('click', rule_page);
 game_button.addEventListener('click', start_game);
+head_game_button.addEventListener('click', start_game);
